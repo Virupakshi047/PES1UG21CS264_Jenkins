004b3275d7b4f7ac886746648aaa493d9f28cd82
Virupakshi047 <144578836+Virupakshi047@users.noreply.github.com>
2024-03-08 20:50:41 +0530

    pesu updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "new change\n";
+  cout << "Pes University\n";
   cout << "PES1UG21CS264\n";
   cout << "I have successfully built and run\n";
   return 0;
